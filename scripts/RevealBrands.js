390fcf16b36554a85a5c366b8119d16d7d650b7b
Colin Gillette <colingillette35@gmail.com>
2018-04-02 18:09:32 -0500

    scripts folder

---
+++ scripts/RevealBrands.js
@@ -0,0 +1,305 @@
+var amberShow = false;
+var blondeShow = false;
+var brownShow = false;
+var creamShow = false;
+var darkShow = false;
+var fruitShow = false;
+var goldenShow = false;
+var honeyShow = false;
+var ipaShow = false;
+var lightShow = false;
+var limeShow = false;
+var paleShow = false;
+var pilsnerShow = false;
+var redShow = false;
+var strongShow = false;
+var wheatShow = false;
+
+
+function amberClick() {
+  var amber = document.getElementById("amber-reveal");
+  var button = document.getElementById("amber-button");
+
+  if (amberShow == false) {
+    amber.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    amberShow = true;
+  }
+  else {
+    amber.innerHTML = "";
+    button.style.backgroundColor = "#ff6600"
+    button.style.color = "white";
+    amberShow = false;
+  }
+}
+
+function blondeClick() {
+  var blonde = document.getElementById("blonde-reveal");
+  var button = document.getElementById("blonde-button");
+
+  if (blondeShow == false) {
+    blonde.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    blondeShow = true;
+  }
+  else {
+    blonde.innerHTML = "";
+    button.style.backgroundColor = "#ffcc00"
+    button.style.color = "white";
+    blondeShow = false;
+  }
+}
+
+function brownClick() {
+  var brown = document.getElementById("brown-reveal");
+  var button = document.getElementById("brown-button");
+
+  if (brownShow == false) {
+    brown.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    brownShow = true;
+  }
+  else {
+    brown.innerHTML = "";
+    button.style.backgroundColor = "#b33c00";
+    button.style.color = "white";
+    brownShow = false;
+  }
+}
+
+function creamClick() {
+  var cream = document.getElementById("cream-reveal");
+  var button = document.getElementById("cream-button");
+
+  if(creamShow == false) {
+    cream.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    creamShow = true;
+  }
+  else {
+    cream.innerHTML = "";
+    button.style.backgroundColor = "#ffd633";
+    button.style.color = "white";
+    creamShow = false;
+  }
+}
+
+function darkClick() {
+  var dark = document.getElementById("dark-reveal");
+  var button = document.getElementById("dark-button");
+
+  if(darkShow == false) {
+    dark.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    darkShow = true;
+  }
+  else {
+    dark.innerHTML = "";
+    button.style.backgroundColor = "#663300";
+    button.style.color = "white";
+    darkShow = false;
+  }
+}
+
+function fruitClick() {
+  var fruit = document.getElementById("fruit-reveal");
+  var button = document.getElementById("fruit-button");
+
+  if(fruitShow == false) {
+    fruit.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    fruitShow = true;
+  }
+  else {
+    fruit.innerHTML = "";
+    button.style.backgroundColor = "#cc3300";
+    button.style.color = "white";
+    fruitShow = false;
+  }
+}
+
+function goldenClick() {
+  var golden = document.getElementById("golden-reveal");
+  var button = document.getElementById("golden-button");
+
+  if(goldenShow == false) {
+    golden.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    goldenShow = true;
+  }
+  else {
+    golden.innerHTML = "";
+    button.style.backgroundColor = "#ffcc00";
+    button.style.color = "white";
+    goldenShow = false;
+  }
+}
+
+function honeyClick() {
+  var honey = document.getElementById("honey-reveal");
+  var button = document.getElementById("honey-button");
+
+  if(honeyShow == false) {
+    honey.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    honeyShow = true;
+  }
+  else {
+    honey.innerHTML = "";
+    button.style.backgroundColor = "#e6b800";
+    button.style.color = "white";
+    honeyShow = false;
+  }
+}
+
+function ipaClick() {
+  var ipa = document.getElementById("ipa-reveal");
+  var button = document.getElementById("ipa-button");
+
+  if(ipaShow == false) {
+    ipa.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    ipaShow = true;
+  }
+  else {
+    ipa.innerHTML = "";
+    button.style.backgroundColor = "#cc6600";
+    button.style.color = "white";
+    ipaShow = false;
+  }
+}
+
+function lightClick() {
+  var light = document.getElementById("light-reveal");
+  var button = document.getElementById("light-button");
+
+  if(lightShow == false) {
+    light.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    lightShow = true;
+  }
+  else {
+    light.innerHTML = "";
+    button.style.backgroundColor = "#ffdb4d";
+    button.style.color = "white";
+    lightShow = false;
+  }
+}
+
+function limeClick() {
+  var lime = document.getElementById("lime-reveal");
+  var button = document.getElementById("lime-button");
+
+  if(limeShow == false) {
+    lime.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    limeShow = true;
+  }
+  else {
+    lime.innerHTML = "";
+    button.style.backgroundColor = "#99cc00";
+    button.style.color = "white";
+    limeShow = false;
+  }
+}
+
+function paleClick() {
+  var pale = document.getElementById("pale-reveal");
+  var button = document.getElementById("pale-button");
+
+  if(paleShow == false) {
+    pale.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    paleShow = true;
+  }
+  else {
+    pale.innerHTML = "";
+    button.style.backgroundColor = "#ffcc66";
+    button.style.color = "white";
+    paleShow = false;
+  }
+}
+
+function pilsnerClick() {
+  var pilsner = document.getElementById("pilsner-reveal");
+  var button = document.getElementById("pilsner-button");
+
+  if(pilsnerShow == false) {
+    pilsner.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    pilsnerShow = true;
+  }
+  else {
+    pilsner.innerHTML = "";
+    button.style.backgroundColor = "#ffd11a";
+    button.style.color = "white";
+    pilsnerShow = false;
+  }
+}
+
+function redClick() {
+  var red = document.getElementById("red-reveal");
+  var button = document.getElementById("red-button");
+
+  if(redShow == false) {
+    red.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    redShow = true;
+  }
+  else {
+    red.innerHTML = "";
+    button.style.backgroundColor = "#ff5c33";
+    button.style.color = "white";
+    redShow = false;
+  }
+}
+
+function strongClick() {
+  var strong = document.getElementById("strong-reveal");
+  var button = document.getElementById("strong-button");
+
+  if(strongShow == false) {
+    strong.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    strongShow = true;
+  }
+  else {
+    strong.innerHTML = "";
+    button.style.backgroundColor = "#663300";
+    button.style.color = "white";
+    strongShow = false;
+  }
+}
+
+function wheatClick() {
+  var wheat = document.getElementById("wheat-reveal");
+  var button = document.getElementById("wheat-button");
+
+  if(wheatShow == false) {
+    wheat.innerHTML = "This is a test";
+    button.style.backgroundColor = "ivory";
+    button.style.color = "black";
+    wheatShow = true;
+  }
+  else {
+    wheat.innerHTML = "";
+    button.style.backgroundColor = "#ffd11a";
+    button.style.color = "white";
+    wheatShow = false;
+  }
+}
